855fc59e5de71ff46c8a7c413422a6333db8e6d1
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-04 19:06:44 -0400

    Create routes and generate snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const tree = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  expect(tree).toMatchSnapshot();
 });
